620afad52b0aca3310d60420d93980e6cbda547a
Tissmax <mathis.basier@gmail.com>
2026-04-13 15:19:33 +0200

    Initial commit

---
+++ src/main/kotlin/com/mathis/livres/LivresApplication.kt
@@ -0,0 +1,11 @@
+package com.mathis.livres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LivresApplication
+
+fun main(args: Array<String>) {
+    runApplication<LivresApplication>(*args)
+}
