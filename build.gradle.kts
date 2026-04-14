f0a8d3d6760272e833c933244a697f103a502ab7
Tissmax <mathis.basier@gmail.com>
2026-04-14 09:58:01 +0200

    Fix de Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,10 @@ tasks.test {
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 pitest {
