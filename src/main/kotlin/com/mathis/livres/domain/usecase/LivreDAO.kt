f47cfd07c531467e2adfcd4e238423cebc8ce610
Tissmax <mathis.basier@gmail.com>
2026-04-14 10:15:00 +0200

    Fix du code du livreDAO

--- src/main/kotlin/com/mathis/livres/domain/usecase/LivreDAO.kt
+++ src/main/kotlin/com/mathis/livres/domain/usecase/LivreDAO.kt
@@ -4,15 +4,21 @@ import com.mathis.livres.domain.models.Livre
 import com.mathis.livres.domain.port.LivreRepository
 
 class LivreDAO: LivreRepository {
-    var livres = mutableListOf<Livre>()
-    override fun fetchAllLivres(): List<Livre> {
-        return livres.sortedBy { it.title }
-    }
+    val livres = mutableListOf<Livre>()
 
     override fun addLivre(livre: Livre) {
-        if (livre.author.isBlank() || livre.title.isBlank()) {
+        if (livre.title.isBlank() || livre.author.isBlank()) {
             throw IllegalArgumentException("Livre author or title missing")
         }
         livres.add(livre)
+    }
+
+    override fun fetchAllLivres(): List<Livre> {
+        // C'est ici que le test de propriété échouait probablement !
+        return livres.sortedBy { it.title.lowercase() }
+    }
+
+    fun clear() {
+        livres.clear()
     }
 }
